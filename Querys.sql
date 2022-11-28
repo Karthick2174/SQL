14de6ef81a2692ba636ae89156b8df829a33c2fb
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2022-11-28 19:09:09 +0530

    Update Querys.sql

--- Querys.sql
+++ Querys.sql
@@ -1,3 +1,3 @@
 SELECT * FROM SchemaName.TableName;
-CREATE TABLE TableName ColoumName1 ColounType NULL OR NOT NULL OR DEFALUT,
+CREATE TABLE TableName ColoumName1 ColounType NULL OR NOT NULL OR DEFAULT,
 ColoumName2 ColoumType2;
