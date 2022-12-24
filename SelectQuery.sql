268219254290560abd6eab3c861c0576f3034d7c
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2022-12-24 19:27:16 +0530

    Create SelectQuery.sql

---
+++ SelectQuery.sql
@@ -0,0 +1,14 @@
+-- Select All Coloum
+SELECT * FROM table_name;
+
+-- Select Particular Coloum
+SELECT coloum_name FROM table_name;
+
+-- Select Start From 'A'
+SELECT * FROM table_name WHERE start_name 'A%';
+
+-- Select Ends With 'A'
+SELECT * FROM table_name WHERE ends_name '%A';
+
+-- Select particular name
+SELECT * FROM table_name WHERE name = 'particularName';
